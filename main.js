839ada82344b042c0efc9a8d13ac6125cbcfde0a
yousseflach <lachgar.youssef6174@gmail.com>
2025-01-08 22:43:14 +0100

    the popular section designed

--- main.js
+++ main.js
@@ -23,6 +23,33 @@ navLink.forEach(link => {
 
 
 /*~~~~~~~~~~~~~~~ SWIPER ~~~~~~~~~~~~~~~*/
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    speed: 400,
+    spaceBetween: 30,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false
+    },
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    grabCursor: true,
+    breakpoints: {
+        640: {
+            slidesPerView: 1
+        },
+        768: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        },
+    }
+  });
 
 
 /*~~~~~~~~~~~~~~~ SCROLL SECTIONS ACTIVE LINK ~~~~~~~~~~~~~~~*/
